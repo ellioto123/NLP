92a515b58525b32c7fa04229a5b68398b19d5c52
ellioto123 <95048286+ellioto123@users.noreply.github.com>
2024-11-20 17:33:58 +0000

    made changes to regex so that retrieving all files and not just 3500

--- DataSplit.py
+++ DataSplit.py
@@ -5,19 +5,21 @@ import glob
 import re
 
 def load_data():
-# Define the path to the folder containing the files
+    # Define the path to the folder containing the files
     neg_folder_path = 'C:/Users/ellio/OneDrive/Documents/Computer Science/NLP/neg'
     pos_folder_path = 'C:/Users/ellio/OneDrive/Documents/Computer Science/NLP/pos'
+    
     # Initialize an empty array
     neg_text_array = []
     pos_text_array = []
     negLabels = []
     posLabels = []
+    
     # Use glob to find all text files in the folder
     for file_path in glob.glob(os.path.join(neg_folder_path, '*.txt')):
         # Extract the rating from the filename using a regular expression
         # Assuming the filename format is something like 'movie_7.txt' where 7 is the rating
-        match = re.search(r'_(\d)\.txt$', os.path.basename(file_path))
+        match = re.search(r'_(\d+)\.txt$', os.path.basename(file_path))
         if match:
             rating = int(match.group(1))
             negLabels.append(0)
@@ -27,10 +29,9 @@ def load_data():
                 content = file.read()
                 neg_text_array.append(content)
 
-
     for file_path in glob.glob(os.path.join(pos_folder_path, '*.txt')):
-        # Open each file and read its content
-        match = re.search(r'_(\d)\.txt$', os.path.basename(file_path))
+        # Extract the rating from the filename using a regular expression
+        match = re.search(r'_(\d+)\.txt$', os.path.basename(file_path))
         if match:
             rating = int(match.group(1))
             posLabels.append(1)
@@ -51,21 +52,12 @@ def load_data():
     X_test, X_eval, y_test, y_eval = train_test_split(X_test, y_test, test_size=0.5, random_state=42)
     return X_train, X_test, X_eval, y_train, y_test, y_eval
 
-# # Print the sizes of the training, testing, and evaluation sets
-# print("Training set size:", len(X_train))
-# print("Testing set size:", len(X_test))
-# print("Evaluation set size:", len(X_eval))
-
-# print(X_train[0], y_train[0])
 
 
 
+# Tokenize the text
 def tokenize(text):
-    for x in range (len(text)):
+    for x in range(len(text)):
         text[x] = text[x].lower()
-        text[x] = re.sub(r'[^\w\s]', '', text[x]) #removes punctuation
+        text[x] = re.sub(r'[^\w\s]', '', text[x])  # removes punctuation
         text[x] = text[x].split(" ")
-
-# def dropTokens(tokens):
-    
-#     return 
