ed3c30480e683f35dbff766983421b68728a7d1c
ellioto123 <95048286+ellioto123@users.noreply.github.com>
2024-11-25 00:00:43 +0000

    Adding POSTagging and constituency parsing

---
+++ PoSTagging.py
@@ -0,0 +1,53 @@
+import nltk
+import pandas as pd
+import numpy as np
+from nltk import RegexpParser
+from sklearn.feature_extraction import DictVectorizer
+
+def posTag(reviewset):
+    posTaggedTokens = []
+    for review in reviewset:
+        posTaggedTokens.append(nltk.pos_tag(review))
+
+    return posTaggedTokens
+
+grammar = "NP: {<DT>?<JJ>*<NN>+}"
+
+def constParse(taggedReviews, grammar):
+    parser = RegexpParser(grammar)
+    parsedReviews = []
+    for review in taggedReviews:
+        parsedReviews.append(parser.parse(review))
+
+    return parsedReviews
+
+def getcountNP(parsedReviews):
+    countNP = []
+    for review in parsedReviews:
+        count = 0
+        for subtree in review.subtrees():
+            if subtree.label() == 'NP':
+                count += 1
+        countNP.append(count)
+
+    return countNP
+
+
+
+
+def combine_matrices(tfidfmatrix, countNP_matrix):
+    countNP_matrix = np.array(countNP_matrix)[:, np.newaxis] #make sure npmatrix is a column vector
+    countNP_matrix = pd.DataFrame(countNP_matrix,columns=['CountNP'])
+    combined_df = pd.concat([tfidfmatrix, countNP_matrix], axis=1)
+    return combined_df
+
+def run(reviewset):
+    taggedReviews = posTag(reviewset)
+    parsedReviews = constParse(taggedReviews, grammar)
+    countNPs = getcountNP(parsedReviews)
+    return countNPs
+
+
+
+
+
